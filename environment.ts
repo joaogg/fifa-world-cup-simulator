dfe99ce4e67e72bb74349f201fb0e3d4161de55b
João Gabriel <joaogabrieldev@gmail.com>
2021-07-29 21:09:26 -0300

    Fixes

--- environment.ts
+++ environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAzLJlEdsMjefcneqLdFm_-yWetE_Uvn9A",
-    authDomain: "crud-fa7ee.firebaseapp.com",
-    databaseURL: "https://crud-fa7ee.firebaseio.com",
-    projectId: "crud-fa7ee",
-    storageBucket: "crud-fa7ee.appspot.com",
-    messagingSenderId: "954386397450",
-    appId: "1:954386397450:web:dc0a41fe87353befb862f3",
-    measurementId: "G-PWJPYJMQE3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
